93ac2dc393fe5b4dac56b67e4157692a74efd437
Yago <yagolopesdefreitas0@gmail.com>
2026-05-17 18:01:14 -0300

    fix: libera hosts no settings para o deploy

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DJANGO_DEBUG", "true").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split()
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1 *").split()
 
 INSTALLED_APPS = [
     "django.contrib.admin",
